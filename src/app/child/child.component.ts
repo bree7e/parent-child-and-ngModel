f50947a46ef9dac71d4e72984c74a350141d91f7
bree7e <vetrov@icc.ru>
2017-11-14 20:11:10 +0800

    evrethig is working perfect

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OnInit, OnChanges, SimpleChange  } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
+  @Input() value: string;
+  @Output() onValueChanged = new EventEmitter<string>();
+  changeLog: string[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  emitData() {
+    this.onValueChanged.emit(`Hello from child with '${this.value}'`);
   }
 
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    const log: string[] = [];
+    for (const propName in changes) {
+      if (changes.hasOwnProperty(propName)) {
+        const changedProp = changes[propName];
+        const to = JSON.stringify(changedProp.currentValue);
+        if (changedProp.isFirstChange()) {
+          log.push(`Initial value of "${propName}" set to ${to}`);
+        } else {
+          const from = JSON.stringify(changedProp.previousValue);
+          log.push(`Variable "${propName}" changed from ${from} to ${to}`);
+        }
+      }
+    }
+    this.changeLog.push(log.join(', '));
+  }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent {
+  passedValue = 'Start';
+  version = 0;
+  changeList: string[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  newVersion() {
+    this.version++;
+    this.passedValue = 'String ' + this.version;
   }
 
+  clear() {
+    this.passedValue = '';
+  }
+
+  handleChange(emmitedData: string) {
+    this.changeList.push(emmitedData);
+    this.clear(); // очень важная для нас строчка
+  }
 }
